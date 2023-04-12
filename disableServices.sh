6db8c2a915bebdd8b1e52a3ca3b32c22b87cacaa
A Z <954997+znetlab@users.noreply.github.com>
2023-04-12 10:25:34 -0700

    Organized

--- disableServices.sh
+++ disableServices.sh
@@ -1,11 +1,10 @@
-# Stop, disable and remove postfix
-# after installing turnkey core
+# Stop, disable, and remove postfix after installing turnkey core
 systemctl stop postfix
 systemctl disable postfix
 apt purge postfix && apt autoremove -y
 
 # check if port 53 is already being used by resolve
-netstat -lntup | grep ":53"
+# netstat -lntup | grep ":53"
 
 
 #disable resolv
